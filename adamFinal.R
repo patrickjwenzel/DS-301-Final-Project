7a9ca062ec9dfc37af78dc6a3034aec31eb4f930
Adam Ford <adamford@iastate.edu>
2021-05-03 16:17:25 -0500

    Neural net added in adamFinal

--- adamFinal.R
+++ adamFinal.R
@@ -1,6 +1,9 @@
 police = read.csv("~/Classwork/D S 301/DS-301-Final-Project/police_killings.csv", header=TRUE)
 library(tidyr)
+library(leaps)
+
 police = police[!police$name == "Unknown",]
+police = police[!is.na(police$college),]
 set.seed(7)
 train = sample(1:nrow(police),nrow(police)/2, replace=FALSE)
 test = (-train)
@@ -8,34 +11,71 @@ test = (-train)
 police$age = as.numeric(police$age)
 meanAge = mean(police$age, na.rm = TRUE)
 police$age = replace_na(police$age, meanAge)
+police$age = scale(police$age)
 
 police$pov = as.numeric(police$pov)
 meanPov = mean(police$pov, na.rm = TRUE)
 police$pov = replace_na(police$pov, meanPov)
 
-police$college = scale(police$college)[1,]
+police$college = scale(police$college)
 
-police$urate = scale(police$urate)[1,]
-police$urate
+police$urate = scale(police$urate)
+
+police$pov = scale(police$pov)
 
 police$white = as.numeric(police$raceethnicity == "White")
 
 
-glm.fit = glm(white~age+gender, data=police,subset=train,family='binomial')
+glm.fit = glm(white~age+gender+armed, data=police,subset=train,family='binomial')
 glm.prob = predict(glm.fit,police[test,],type='response')
 glm.pred = rep(0,length(test))
 glm.pred[glm.prob >0.5] = 1
 table(glm.pred,police[test,]$white)
-1-mean(glm.pred == police[test,]$white)
+mean(glm.pred == police[test,]$white)
 
 
 #No important type of error to avoid, neither is more important
 
 
-glm.fit = glm(white~college+urate+pov, data=police,subset=train,family='binomial')
+glm.fit = glm(white~college*urate*pov, data=police,subset=train,family='binomial')
 glm.prob = predict(glm.fit,police[test,],type='response')
 glm.pred = rep(0,length(test))
 glm.pred[glm.prob >0.5] = 1
 table(glm.pred,police[test,]$white)
-1-mean(glm.pred == police[test,]$white)
+mean(glm.pred == police[test,]$white)
 
+regfit = regsubsets(white~age+gender+armed+college*urate*pov, data=police, nvmax=6)
+n = dim(police)[1]
+regfit.sum = summary(regfit)
+p = rowSums(regfit.sum$which) #number of predictors in the model 
+adjr2 = regfit.sum$adjr2
+cp = regfit.sum$cp
+rss = regfit.sum$rss
+AIC = n*log(rss/n) + 2*(p)
+BIC = n*log(rss/n) + (p)*log(n)
+barplot(AIC)
+barplot(BIC)
+plot(adjr2)
+plot(cp)
+
+coef(regfit, 4)
+
+glm.fit = glm(white~armed+age+college*urate*pov, data=police,subset=train,family='binomial')
+glm.prob = predict(glm.fit,police[test,],type='response')
+glm.pred = rep(0,length(test))
+glm.pred[glm.prob >0.5] = 1
+table(glm.pred,police[test,]$white)
+mean(glm.pred == police[test,]$white)
+
+library(neuralnet)
+police$inter <- police$college * police$urate * police$pov
+n = neuralnet(white~age+inter, data=police[train,], hidden=c(2,1), linear.output=FALSE, threshold=0.01)
+plot(n)
+temp = subset(police[test,], select = c(age, inter))
+n.results = compute(n, temp)
+res = data.frame(actual = police[test,]$white, prediction = n.results$net.result)
+rounded = sapply(res, round, digits=0)
+roun = data.frame(rounded)
+attach(roun)
+table(actual, prediction)
+mean(roun$actual == roun$prediction)
